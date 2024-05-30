1cf28e8485cb12171cee8eabec4a27c04e2dcd85
minhja <minh_nguyen2@bmc.com>
2024-05-30 12:43:47 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,37 +3,44 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://minhion.github.io',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Use Cases',
-					items: [
-						{ label: 'AWS', link: '/guides/aws/' },
-						autogenerate: { directory: '/guides/aws' },
-						{ label: 'Azure', link: '/guides/azure/' },
-						autogenerate: { directory: '/guides/azure' },
-						{ label: 'GCP', link: '/guides/gcp/' },
-						autogenerate: { directory: '/guides/gcp' }
-					]
-				}
-			],
-		}),
-	],
+  site: 'https://minhion.github.io',
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+        {
+          label: 'Use Cases',
+          items: [
+            { label: 'AWS', link: '/guides/aws/' },
+          ],
+        },
+        {
+          label: 'AWS Use Cases',
+          autogenerate: { directory: 'guides/aws' },
+        },
+        {
+          label: 'Azure Use Cases',
+          autogenerate: { directory: 'guides/azure' },
+        },
+        {
+          label: 'GCP Use Cases',
+          autogenerate: { directory: 'guides/gcp' },
+        },
+      ],
+    }),
+  ],
 });
